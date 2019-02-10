6b9dc6ac2d37e3950459fb33e6eb9b5de04a8eab
unknown <2107sachin@gmail.com>
2019-02-10 19:21:51 +0530

    locally added doesNotExist flat

--- Demo/src/com/infy/Hello.java
+++ Demo/src/com/infy/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("First line of program");
-		boolean copied = true;
+		boolean copied = true , doesNotExist = false;
 		if(copied)
 		{
 			System.out.println("I am in copy branch...");
