f33ab92b8cb22cf0a0a19dda78e638ac352284cf
unknown <2107sachin@gmail.com>
2019-02-10 18:29:53 +0530

    committed in copied

--- Demo/src/com/infy/Hello.java
+++ Demo/src/com/infy/Hello.java
@@ -4,5 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("First line of program");
+		boolean copied = true;
+		if(copied)
+		{
+			System.out.println("I am in copy branch...");
+		}else
+		{
+			System.out.println("I am in master branch...");
+		}
 	}
 }
