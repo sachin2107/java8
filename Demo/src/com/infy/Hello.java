f9ae24256602063d32aeb939ecbd3140a23f68f5
sachin2107 <34779103+sachin2107@users.noreply.github.com>
2019-02-10 19:19:50 +0530

    creating conflict by adding doesnot exist

--- Demo/src/com/infy/Hello.java
+++ Demo/src/com/infy/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("First line of program");
 		boolean copied = true;
+		boolean doesNotExist = false;
 		if(copied)
 		{
 			System.out.println("I am in copy branch...");
